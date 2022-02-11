a8b9b8f50940138f973a9e462f1da868c0562029
Aimee <anortje@babbel.com>
2022-02-11 09:59:18 +0100

    add first test suite

---
+++ src/components/App/App.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { render, screen } from "@testing-library/react";
+import App from 'components/App'
+
+it('should render the app', () => {
+    render(<App/>);
+
+})
