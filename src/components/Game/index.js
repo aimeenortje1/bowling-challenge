b4f7f607f79b95ee41ebef558cdf45ce587ff637
Aimee <anortje@babbel.com>
2022-02-11 15:35:27 +0100

    update strike and spare scores

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,50 +1,94 @@
 import React, { useState, useEffect } from "react";
 import FramesList from "components/FramesList";
 import { emptyFrames } from "utils/emptyFrames";
+import {
+  getRandomPins,
+  calculateFrameScore,
+  updateStrikeScore,
+  updateSpareScore,
+  updateFrameScore,
+} from "utils/";
+import { SPARE, STRIKE } from "utils/types";
+
 import "./Game.scss";
 
 const Game = () => {
   const [frames, setFrames] = useState(emptyFrames);
   const [frameIndex, setFrameIndex] = useState(0);
-  const incrementFrameIndex = () => setFrameIndex(frameIndex + 1);
   const [gameScore, setGameScore] = useState(0);
+
+  const incrementFrameIndex = () => setFrameIndex(frameIndex + 1);
 
   //   updateGameScore
   useEffect(() => {
-    const prevFrameScore = frames[frameIndex - 1]?.rolls.reduce(
-      (roll, prevRoll) => roll + prevRoll
-    );
-    if (prevFrameScore) {
+    
+    if (!frames[frameIndex - 1]?.rolls) return;
+    const prevFrameScore = calculateFrameScore(frames[frameIndex - 1]?.rolls);
+    if (frames[frameIndex-1].bonus === null &&  prevFrameScore) {
       setGameScore(gameScore + prevFrameScore);
+      setFrames((state) => updateFrameScore(state, frameIndex));
     }
   }, [frameIndex]);
 
-  const getRandomPins = (alreadyDown = 0, forced = 0) => {
-    return Math.floor(Math.random() * (10 - alreadyDown));
-  };
+  //   handle previous strike
+  useEffect(() => {
+    if (frames[frameIndex - 2]?.bonus === STRIKE) {
+      console.log("STRIKE SCORE UPDATE!");
+      setFrames((state) => {
+        return updateStrikeScore(state, frameIndex);
+      });
+    } 
+  }, [frameIndex]);
 
-  const rollFirst = () => {
-    const pins = getRandomPins();
+  //   handle previous spare
+  useEffect(() => {
+    //   for the sake of readability.
+    const currentFrame = frames[frameIndex];
+    const prevFrame = frames[frameIndex - 1];
+
+    const shouldUpdateSpare = () =>
+      prevFrame?.bonus === SPARE && currentFrame?.rolls[0] !== null
+        ? true
+        : false;
+
+    // rolling a strike immediately after a spare does not work:
+    // if previous frame was a strike, the frameIndex would have updated already, meaning we're missing the spare score update
+
+    if (shouldUpdateSpare()) {
+      console.log("SPARE SCORE UPDATE!");
+      setFrames((state) => {
+        return updateSpareScore(state, frameIndex);
+      });
+    }
+  }, [frames[frameIndex]]);
+
+  const rollFirst = (forcedPins) => {
+    const pins = getRandomPins(0, forcedPins);
+    const isStrike = pins === 10 ? true : false;
 
     setFrames((state) => {
       const newState = [
         ...state.slice(0, frameIndex),
         {
           index: frameIndex,
-          rolls: [pins, null],
+          rolls: [pins, isStrike ? "x" : null],
           score: null,
-          bonus: null,
+          bonus: isStrike ? STRIKE : null,
         },
         ...state.slice(frameIndex, -1),
       ];
-      console.log(newState);
-
+      //   console.log(newState);
+      if (isStrike) {
+        incrementFrameIndex();
+      }
       return newState;
     });
   };
 
-  const rollSecond = () => {
-    const pins = getRandomPins(frames[frameIndex]?.rolls[0]);
+  const rollSecond = (forcedPins) => {
+    const previousRoll = frames[frameIndex]?.rolls[0];
+    const pins = getRandomPins(previousRoll, forcedPins);
+    const isSpare = previousRoll + pins === 10 ? true : false;
 
     setFrames((state) => {
       const newState = [
@@ -52,10 +96,11 @@ const Game = () => {
         {
           ...state[frameIndex],
           rolls: [state[frameIndex].rolls[0], pins],
+          bonus: isSpare ? SPARE : null,
         },
         ...state.slice(frameIndex + 1),
       ];
-      console.log(newState);
+      //   console.log(newState);
 
       return newState;
     });
